c74873be647ad68251a6b498f69b258e63c1d030
Juan Gálvez <143042118+JuanG4lvez@users.noreply.github.com>
2024-11-11 01:25:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./components/Home";
-import Checkout from "./components/Checkout";
-import Footer from "./components/Footer";
-import { SignIn } from "./pages/SignIn";
-
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/login" element={<SignIn />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
-    </>
-  );
-=======
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
